e5a2ab54301f581c07e14eb598eeb723d2500a92
Mateusz Kufrasa <mateusz.kufrasa@gmail.com>
2023-01-09 23:34:38 +0100

    2nd commit

--- FileReader.py
+++ FileReader.py
@@ -1,4 +1,7 @@
 import csv
+import random
+
+from Utilities import Utilities
 
 
 class FileReader():
@@ -35,3 +38,28 @@ class FileReader():
     def print_data(self, start=0, end=None):
         end = len(self.dataset) if end is None or end > len(self.dataset) else end
         print(self.dataset[start:end])
+
+    def split_dataset_into(self, filereader, train=80, test=10, valid=10):
+        try:
+            train = int(train)
+            test = int(test)
+            valid = int(valid)
+        except:
+            print('Podano niewlasciwe wartosci')
+        if train + test + valid == 100:
+            index_range = list(range(len(filereader)))
+            train_indexes = random.sample(index_range, round(train * len(index_range) / 100))  # wybiera liste indeksow dla zespolu treningowego
+            train_set = [filereader[x] for x in train_indexes]
+            filereader = [filereader[x] for x in train_indexes if x not in filereader]
+            index_range = list(range(len(filereader)))
+            test_indexes = random.sample(index_range, round(test * len(index_range) / 100))  # wybiera liste indeksow dla zespolu testowego
+            test_set = [filereader[x] for x in test_indexes]
+            filereader = [filereader[x] for x in test_indexes if x not in filereader]
+            index_range = list(range(len(filereader)))
+            valid_indexes = random.sample(index_range, round(valid * len(index_range) / 100))  # wybiera liste indeksow dla zespolu testowego
+            valid_set = [filereader[x] for x in valid_indexes]
+            filereader = [filereader[x] for x in valid_indexes if x not in filereader]
+            print(train_set,test_set,valid_set)
+
+        else:
+            print('Podano niepoprawne wartosci parametrow')
--- run.py
+++ run.py
@@ -2,15 +2,18 @@ from FileReader import FileReader
 
 
 def main():
+    f = FileReader()
     try:
         file_path, if_headers = input(
             'Podaj ścieżkę pliku oraz wartość 1 gdy plik zawiera nagłówek, lub 0 gdy nie zawiera: ').split()
-        f = FileReader()
         f.load_dataset(file_path, if_headers)
         f.print_header()
         f.print_data()
     except ValueError:
         print('Błędne argumenty')
+    train, test, valid = input(
+            'Podaj rpocentowe proporcje zbioru treningowego, testowego i walidacyjnego: ').split()
+    f.split_dataset_into(f.dataset, train, test, valid)
 
 
 main()
